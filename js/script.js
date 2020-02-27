5809292b185000260ae224fee17e0b9394a3a0c1
Marie-Léa Côté <marielea.cote@gmail.com>
2020-02-27 10:40:48 -0500

    Finished up css for menu, added click in container to close menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+// ====== Menu ====== 
+const menus = document.querySelectorAll(".has-submenu");
+menus.forEach(menu => {
+
+	menu.addEventListener("click", function () {
+		clearMenuSelection(menu);
+		menu.nextElementSibling.classList.toggle("is-open");
+		menu.parentElement.classList.toggle("is-open");
+		menu.blur();
+	}
+	);
+	menu.addEventListener("keypress", function (e) {
+		if (e.key == "Enter") {
+			clearMenuSelection(menu);
+			menu.nextElementSibling.classList.toggle("is-open");
+			menu.parentElement.classList.toggle("is-open");
+		}
+	}
+	);
+
+});
+
+// Closes opened submenus
+const clearMenuSelection = (menu) => {
+	menus.forEach(submenu => {
+		if (submenu != menu) {
+			submenu.nextElementSibling.classList.remove("is-open");
+			submenu.parentElement.classList.remove("is-open");
+		}
+	});
+}
+
+// Closes opened menu when clicking in main container
+// Didn't do on the body since it messed with the toggle
+document.querySelector(".main-container").addEventListener("click", function () { clearMenuSelection(menu); }, true);
+
 // ====== Tooltips ====== 
 const tooltips = document.querySelectorAll(".info");
 
@@ -16,28 +52,3 @@ tooltips.forEach(tooltip => {
 	);
 });
 
-const menus = document.querySelectorAll(".has-submenu");
-menus.forEach(menu => {
-	menu.addEventListener("click", function () {
-		clearMenuSelection(menu);
-		menu.nextElementSibling.classList.toggle("is-open");
-		menu.blur();
-	}
-	);
-	menu.addEventListener("keypress", function (e) {
-		if (e.key == "Enter") {
-			clearMenuSelection(menu);
-			menu.nextElementSibling.classList.toggle("is-open");
-		}
-	}
-	);
-
-});
-
-const clearMenuSelection = (menu) => {
-	menus.forEach(submenu => {
-		if(submenu != menu) {
-		submenu.nextElementSibling.classList.remove("is-open");
-		}
-	});
-}
