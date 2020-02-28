d6169e96b88383523c52282d3cd2b77c028aa5a7
Marie-Léa Côté <marielea.cote@gmail.com>
2020-02-28 12:38:15 -0500

    Finishing clean up

--- js/script.js
+++ js/script.js
@@ -37,13 +37,6 @@ document.querySelector(".main-container").addEventListener("click", function () 
 // ====== Tooltips ====== 
 const tooltips = document.querySelectorAll(".info");
 
-// const toggleTooltip = (tooltip) => {
-// 	tooltip.querySelector(":scope > .tooltip").classList.toggle("tooltip-hidden");
-// 	tooltip.querySelector(":scope > .tooltip-pointer").classList.toggle("tooltip-hidden");	
-// 	console.log(tooltip);
-// }
-
-// to-do: Try to see if you can add a condition to fix the tooltips being out of bounds if too close to an edge
 tooltips.forEach(tooltip => {
 	tooltip.addEventListener("click", function () {
 		tooltip.querySelector(":scope > .tooltip").classList.toggle("tooltip-hidden");
